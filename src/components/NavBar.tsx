863d39ef1f034371f284327d7b9cccf7fadfccfa
rasuru <rasuru@protonmail.com>
2023-05-05 13:29:11 +0600

    Fix HTML list structure in nav bar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,21 +6,19 @@ export function NavBar() {
   return (
     <nav className="mx-auto rounded-full py-2 px-3 bg-[rgb(22,22,43)] bg-opacity-60 font-medium backdrop-blur-lg">
       <ul className="flex space-x-2 transition-[font-size] md:space-x-4">
-        <a
-          href="/"
+        <li
           className={classNames(
             "nav__link px-4 py-1 rounded-full transition-[padding] bg-opacity-30 backdrop-blur after:content-['/'] after:text-[10px] xs:after:text-xs after:inline-flex after:border-2 after:border-indigo-500 after:rounded-md after:h-5 after:pt-0.5 after:w-5 xs:after:w-6 xs:after:h-6 after:items-center after:justify-center after:ml-2 after:align-middle after:border-opacity-40 after:text-indigo-300",
             activeNavBarTab === NavBarTab.Work && "bg-[rgb(50,50,203)]"
           )}
         >
-          <span className="align-middle">Work</span>
-        </a>
-        <a
-          href="/#contact"
-          className="nav__link px-4 py-1 rounded-full transition-[padding]"
-        >
-          Contact
-        </a>
+          <a href="/" className="align-middle">
+            Work
+          </a>
+        </li>
+        <li className="nav__link px-4 py-1 rounded-full transition-[padding]">
+          <a href="/#contact">Contact</a>
+        </li>
       </ul>
     </nav>
   );
